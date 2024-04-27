e14133b508836a401990ec2be42073be6d19ff07
xiaowenzii <xiaowenzi0822@aliyun.com>
2024-04-27 17:14:49 +0800

    导入echart 绘制柱状图

--- miniprogram/pages/functions/energyOverview/energyOverview.js
+++ miniprogram/pages/functions/energyOverview/energyOverview.js
@@ -1,66 +1,118 @@
-// pages/functions/energyOverview/energyOverview.js
+import * as echarts from '../../component/ec-canvas/echarts'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    dateTpye: 0,
+    monthList: ["当月", "上月", "5月", "6月", "7月", "8月"],
+    monthSelected: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  selectDateType(res){
+    var index = res.currentTarget.dataset.index;
+    this.setData({
+      dateTpye: index
+    })
+  },
+  selectMonth(res){
+    var index = res.currentTarget.dataset.index;
+    this.setData({
+      monthSelected: index
+    })
+  },
+  
   onLoad(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady() {
-
+    var energyChart = this.selectComponent('#energy-chart-bar');
+    var xData = ["04-01", "04-02", "04-03", "04-04", "04-05", "04-06", "04-07", "04-08"];
+    var yData = [85, 100, 34, 24, 46, 98, 80, 62];
+    this.drawChart(energyChart, xData, yData)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  // 绘制柱状图
+  drawChart(chartComponnet, xData, yData) {
+    var option = {
+      xAxis: {
+        type: 'category',
+        data: xData,
+        axisLabel:{
+          interval:0
+        }
+      },
+      yAxis: {
+          type: 'value'
+      },
+      series: [{
+        name: '用电数据',
+        type: 'bar',
+        label:{
+          show: true, 
+          position: 'top',
+          formatter: (value,index)=> { 
+            return value?.value;
+          }
+        },
+        itemStyle: {
+          normal: {
+            borderWidth: 1,
+            color: { type: 'linear', x: 1, y: 0, x2: 0,  y2: 1,
+              colorStops: [{
+                offset: 0,
+                color: '#18B6A2' //0% 处的颜色
+              }, {
+                offset: 1,
+                color: '#E7F8F5' //100% 处的颜色
+              }],
+              globalCoord: true, //缺省为false
+            },
+            barBorderRadius: [0, 0, 0, 0], //柱状图radius
+            label: {
+              show: false, //柱状图顶部是否显示数值
+              position: 'top',
+              textStyle: {
+                color: '#222222'
+              },
+              formatter: function (params) {
+                if (params.value == 0) {
+                  return '';
+                } else {
+                  return params.value;
+                }
+              }
+            },
+          },
+        },
+        data: yData
+      }],
+      grid:{
+        x: 36,  //距离左边
+        x2: 24, //距离右边
+        y:24,   //距离上边
+        y2:24,  //距离下边
+      }
+    };
+    const getPixelRatio = () => {
+      let pixelRatio = 0
+      wx.getSystemInfo({
+        success: function (res) {
+          pixelRatio = res.pixelRatio
+        },
+        fail: function () {
+          pixelRatio = 0
+        }
+      })
+      return pixelRatio
+    }
+    var dpr = getPixelRatio()
+    if (chartComponnet) {
+      chartComponnet.init((canvas, width, height) => {
+        const chart = echarts.init(canvas, null, {
+          width: width,
+          height: height,
+          devicePixelRatio: dpr
+        }); 
+        chart.setOption(option, true);
+        return chart;
+      });
+    }
   }
 })
